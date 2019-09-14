4b9d0e8e3b918ac938d35d231e5d38243954730f
Elizabeth Zhou <eazhou@mit.edu>
2019-09-14 13:02:25 -0400

    added ios app project

---
+++ hackmit-2019-app/hackmit-2019-appTests/hackmit_2019_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  hackmit_2019_appTests.swift
+//  hackmit-2019-appTests
+//
+//  Created by Elizabeth Zhou on 9/14/19.
+//  Copyright © 2019 Elizabeth Zhou. All rights reserved.
+//
+
+import XCTest
+@testable import hackmit_2019_app
+
+class hackmit_2019_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
