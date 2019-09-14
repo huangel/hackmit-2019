c1aa26d3008f5f53a899a69653006e7675b7e5d3
Katherine Xiao <klxiao@mit.edu>
2019-09-14 13:07:08 -0400

    kat app

---
+++ hackmit-2019-2/hackmit-2019-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hackmit-2019-2
+//
+//  Created by Katherine Xiao on 9/14/19.
+//  Copyright © 2019 Katherine Xiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
