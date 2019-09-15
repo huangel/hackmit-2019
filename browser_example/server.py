709726dccb4120b8f351d791998746e442b79e76
Angel Huang <huangel@mit.edu>
2019-09-15 10:49:36 -0400

    whatever

--- browser_example/server.py
+++ browser_example/server.py
@@ -36,4 +36,5 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
         socketio.emit('my response', x)
     # socketio.emit('my response', json, callback=messageReceived)
 
-    
+if __name__ == '__main__':
+    app.run()
